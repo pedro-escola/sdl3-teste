969e0c00279e9da2ab6b30b80d7ed749f0b4f4fa
Pedro Augusto <211666853+pedro-escola@users.noreply.github.com>
2026-04-30 10:52:42 -0300

    usa o appstate
    uhullll

--- src/main.c
+++ src/main.c
@@ -7,8 +7,11 @@
     #define VERSION "DEV"
 #endif
 
-static SDL_Window *gWindow = NULL;
-static SDL_Renderer *gRenderer = NULL;
+typedef struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    double delta;
+} AppState;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     if (!SDL_SetAppMetadata("SDL3 Teste", VERSION, "com.example.sdl3")) {
@@ -21,10 +24,18 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
+    AppState *state = SDL_calloc(1, sizeof(AppState));
+    if (!state) {
+        SDL_Log("Initialization failed! Cuz: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    *appstate = state;
+
     const int kScreenWidth = 640;
     const int kScreenHeight = 480;
 
-    if (!SDL_CreateWindowAndRenderer("SDL3 Teste", kScreenWidth, kScreenHeight, 0, &gWindow, &gRenderer)) {
+    if (!SDL_CreateWindowAndRenderer("SDL3 Teste", kScreenWidth, kScreenHeight, 0, &state->window, &state->renderer)) {
         SDL_Log("Window creation failed! Cuz: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -40,23 +51,32 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
+    AppState *state = ((AppState *)appstate);
     const Uint64 currentTime = SDL_GetTicks();
     static Uint64 prevTime = 0;
 
-    SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(gRenderer);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(state->renderer);
 
-    Uint64 delta = currentTime - prevTime;
+    double delta = ((double)(currentTime - prevTime)) / 1000;
 
-    SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    SDL_SetRenderScale(gRenderer, 2, 2);
-    SDL_RenderDebugTextFormat(gRenderer, 0, 0, "Delta: %f", ((float)delta / 1000));
-    SDL_SetRenderScale(gRenderer, 1, 1);
+    SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_SetRenderScale(state->renderer, 2, 2);
+    SDL_RenderDebugTextFormat(state->renderer, 0, 0, "Delta: %f", delta);
+    SDL_SetRenderScale(state->renderer, 1, 1);
 
-    SDL_RenderPresent(gRenderer);
+    SDL_RenderPresent(state->renderer);
     prevTime = currentTime;
 
     return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    if (appstate != NULL) {
+        AppState *state = ((AppState *)appstate);
+
+        SDL_DestroyRenderer(state->renderer);
+        SDL_DestroyWindow(state->window);
+        SDL_free(state);
+    }
+}
